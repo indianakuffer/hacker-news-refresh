9f9a22ec1c90e78d4f7f9aa53f74425b27837f8a
indianakuffer <indianakuffer@gmail.com>
2022-09-29 17:06:58 +0000

    Deploying to public from @ indianakuffer/hacker-news-refresh@ebcd26e05526cbc8426619cd20734965d82b37c6 🚀

--- global/functions.ts
+++
@@ -1,5 +0,0 @@
-export function htmlDecode(str: string) {
-  let withLineBreaks = str.replace(/<p>/g,"\n\n")
-  const doc = new DOMParser().parseFromString(withLineBreaks, "text/html");
-  return doc.documentElement.textContent;
-}
